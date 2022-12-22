b73d5a497f52a7325333405b3ed1f9e77d5bc8b4
Anton <antonch92@gmail.com>
2022-12-22 01:00:24 +0100

    mistakes descriptions are changed

--- src/__tests__/nicksCheckTest.js
+++ src/__tests__/nicksCheckTest.js
@@ -1,10 +1,17 @@
 import Validator from '../js/nicksCheck';
 
 test.each([
-  ['mistake in the start', '1vasya'],
-  ['mistake in the end', 'vasya1'],
-  ['mistake of exceeding digits repeation', 'va1111sya'],
-  ['mistake of using the forbidden symbol', 'va.sya'],
-])('test case when %s with %i value', (_, nickName) => {
+  ['using more than 3 digits in the range', 'va1111sya'],
+  ['using special symbols other than "-" и "_"', 'va.sya'],
+  ['using alphabets other than English', 'вася'],
+  ['starting with number', '1vasya'],
+  ['starting with "-"', '-vasya'],
+  ['starting with "_"', '_vasya'],
+  ['starting with "."', '.vasya'],
+  ['ending with number', 'vasya1'],
+  ['ending with "-"', 'vasya-'],
+  ['ending with "_"', 'vasya_'],
+  ['ending with "."', 'vasya.'],
+])('test case %s with %i value', (_, nickName) => {
   expect(() => new Validator(nickName)).toThrowError();
 });
--- src/js/nicksCheck.js
+++ src/js/nicksCheck.js
@@ -5,8 +5,14 @@ export default class Validator {
   }
 
   validateUsername(name) { // eslint-disable-line
-    if (/([0-9])\1{3}/.test(name) || /[^\w_/-]/.test(name) || /^[0-9.\-_]|[0-9.\-_]$/.test(name)) {
-      throw new Error('Неподходящий формат имени');
+    if (/([0-9])\1{3}/.test(name)) {
+      throw new Error('Недопустимо использовать в имени более 3-х цифр подряд');
+    }
+    if (/[^\w_/-]/.test(name)) {
+      throw new Error('Имя может состоять из латинского алфавита, цифр, символов "-" и "_"');
+    }
+    if (/^[0-9.\-_]|[0-9.\-_]$/.test(name)) {
+      throw new Error('Недопустимо использование цифр, "-", "." и "_" в качестве первого и последнего символов');
     }
   }
 }
